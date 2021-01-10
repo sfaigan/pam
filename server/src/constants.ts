956e9dccad0a943ffea59a2c68f46ba56b898607
Shea Faigan <stfaigan@gmail.com>
2021-01-09 18:45:52 -0800

    Implement StreamingService enum

---
+++ server/src/constants.ts
@@ -0,0 +1,7 @@
+export enum StreamingService {
+  NETFLIX = 0,
+  DISNEY_PLUS = 337,
+  AMAZON_PRIME = 1,
+  CRAVE_STARZ = 6,
+  CRAVE_PLUS = 231,
+}
--- server/src/types.ts
+++ server/src/types.ts
@@ -1,6 +1,1 @@
-export enum StreamingService {
-  NETFLIX = "NETFLIX",
-  DISNEY_PLUS = "DISNEY_PLUS",
-  AMAZON_PRIME = "AMAZON_PRIME",
-  CRAVE = "CRAVE",
-}
+
