139757b5ee74d2560c577ddf358a71b23fe83fc1
Shea Faigan <stfaigan@gmail.com>
2021-01-10 00:16:44 -0800

    Fix StreamingService, add CountryCode

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -1,7 +1,11 @@
 export enum StreamingService {
-  NETFLIX = 0,
+  NETFLIX = 8,
   DISNEY_PLUS = 337,
-  AMAZON_PRIME = 1,
-  CRAVE_STARZ = 6,
-  CRAVE_PLUS = 231,
+  AMAZON_PRIME = 119,
+}
+
+export enum CountryCode {
+  CA = "CA",
+  US = "US",
+  UK = "UK",
 }
