3211f653ee920881995df9d9efc79f3751cc4281
Darian Morrison <darianjmorrison@gmail.com>
2021-01-10 02:33:39 -0800

    Setup group model for new use case

--- server/src/models/group.ts
+++ server/src/models/group.ts
@@ -1,15 +1,30 @@
 import { Document, model, Schema } from "mongoose";
 
 export interface GroupDoc extends Document {
+  count: number;
   code: string;
   movies: Map<string, number>;
+  genre: number[];
+  providers: string[];
 }
 
 const GroupSchema = new Schema({
+  count: {
+    type: Number,
+    default: 1,
+  },
   code: String,
   movies: {
     type: Map,
     of: Number,
+  },
+  genre: {
+    type: Array,
+    of: Number,
+  },
+  providers: {
+    type: Array,
+    of: String,
   },
 });
 
